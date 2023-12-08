bdae07ee8ebacf774b2b4e3585a041fbd896a560
Janderson Goncalves <janderson.goncalves@bkbank.com.br>
2023-12-08 17:34:47 -0300

    update

--- Model/Pizza.cs
+++ Model/Pizza.cs
@@ -24,11 +24,14 @@ namespace BlazingPizza
 
         public List<PizzaTopping> Toppings { get; set; }
 
-        public decimal GetBasePrice()
-        {
-            return ((decimal)Size / (decimal)DefaultSize) * Special.BasePrice;
-        }
-
+        //public decimal GetBasePrice()
+        //{
+        //    return ((decimal)Size / (decimal)DefaultSize) * Special.BasePrice;
+        //}
+        public decimal GetBasePrice() =>
+            Special is { FixedSize: not null }
+                ? Special.BasePrice
+                : (decimal)Size / DefaultSize * Special?.BasePrice ?? 1;
         public decimal GetTotalPrice()
         {
             return GetBasePrice();
